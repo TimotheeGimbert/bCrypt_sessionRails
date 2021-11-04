1210664fe1848426c48eddbef747f55c6966419f
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-04 15:19:08 +0100

    adds access authenticate only #

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,3 +9,25 @@ module SessionsHelper
   end
 
 end
+
+
+
+# RESERVES ACCES TO AUTHENTICATED ONLY :
+
+#class TonController < ApplicationController
+#  before_action :authenticate_user, only: [:index]  PASSE DABORD PAR AUTH AVANT DALLER SUR :INDEX
+
+#  def index
+    # on code quelque chose qui permet d'afficher le dashboard de l'utilisateur
+#  end
+
+#  private
+
+#  def authenticate_user
+#    unless current_user
+#      flash[:danger] = "Please log in."
+#      redirect_to new_session_path
+#     end
+#  end
+
+#end
