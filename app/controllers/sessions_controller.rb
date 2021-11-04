3e334abb0fb0a36608220ff4dd4c5a709d01ddad
TimotheeGimbert <timothee.gimbert@free.fr>
2021-11-04 12:34:01 +0100

    adds deconnexion to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,18 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+    puts '$' * 60
     if user && user.authenticate(params[:password])
+      puts 'SESSIONTRUE' * 10
       session[:user_id] = user.id
     else
+      puts 'SESSIONFALSE' * 10
       flash.now[:danger] = 'Invalid user/pass combo !'
       render 'new'
     end
   end
 
   def destroy
+    session.delete(:user_id)
   end
 end
